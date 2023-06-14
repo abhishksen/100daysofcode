5ec78d27aea4a492408b331eb2e904420283e984
Abhishek <codmonk21@gmail.com>
2023-06-14 17:10:56 +0530

    day 32 commit

--- day-31/src/Hooks/useReducer/UseReducer.jsx
+++ day-31/src/Hooks/useReducer/UseReducer.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseReducer = () => {
+
+  const [count, setCount] = useState(0);
+  const [showText, setShowText] = useState(true);
+
   return (
     <div>
       useReducer hook
+      <h3>{count}</h3>
+      <button onClick={
+        () => {
+          setCount(count+1);
+          setShowText(!showText);
+        }
+      }>Click Me</button>
+      {showText && <h3>Text is shown</h3>}
     </div>
   )
 }
