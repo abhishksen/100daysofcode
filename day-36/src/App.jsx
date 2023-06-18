4dbe78b7556227794104914d421c42895eb99c6e
Abhishek <codmonk21@gmail.com>
2023-06-18 21:52:23 +0530

    project-setup done

--- day-36/src/App.jsx
+++ day-36/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import {FaAirbnb} from 'react-icons/fa';
 
 const App = () => {
   return (
     <div>
       App Component
+      <Button variant="primary">Primary</Button>
+      <FaAirbnb />
     </div>
   )
 }
