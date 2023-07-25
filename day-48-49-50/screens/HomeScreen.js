26fe98696fdf17e9669527a4f306ff050b20b487
Abhishek <codmonk21@gmail.com>
2023-07-25 21:23:12 +0530

    day-50 commit revisited
    need to work in this project

--- day-48-49-50/screens/HomeScreen.js
+++ day-48-49-50/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity, ScrollView, ImageBackground } from 'react-native'
 import React, { useCallback, useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { MagnifyingGlassIcon, XMarkIcon } from 'react-native-heroicons/outline'
@@ -13,23 +13,25 @@ import { getData, storeData } from '../utils/asyncStorage';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 relative">
+    <View className="flex-1 flex-col" >
       <StatusBar style="light" />
-      <Image blurRadius={30} source={require('../assets/images/bgImg.png')}
-        className="absolute h-full w-full"
-      />
-      <SafeAreaView className="flex flex-1">
-        <View style={{ height: '7%' }} className="mx-4 relative z-50">
-          <View className="flex-row justify-end items-center rounded-full"
-            style={{ backgroundColor: theme.bgWhite(0.2) }}>
-            <TextInput
-              placeholder='Search City'
-              placeholderTextColor={'lightgray'}
-              className="pt-6 h-10 flex-1 text-base text-white"
-            />
+      <ImageBackground blurRadius={5} resizeMode='cover' source={require('../assets/images/bgImg.png')}
+        className="flex-1 justify-center"
+        style={{ height: '100%' }}
+      >
+        <SafeAreaView className="flex flex-1">
+          <View style={{ height: '30%', marginLeft:'10px'}}  className="mx-4 relative z-50">
+            <View className="flex-row justify-end items-center rounded-full"
+              style={{ backgroundColor: theme.bgWhite(0.2) }}>
+              <TextInput
+                placeholder='Search City'
+                placeholderTextColor={'lightgray'}
+                className="pt-6 h-10 flex-1 text-base text-white"
+              />
+            </View>
           </View>
-        </View>
-      </SafeAreaView>
+        </SafeAreaView>
+      </ImageBackground>
     </View>
   );
 }
