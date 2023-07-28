33938e01264aa9a74b75e988a951b0f16bf2f958
Abhishek <codmonk21@gmail.com>
2023-07-28 17:25:22 +0530

    initial commit day 54
    login functionality with cookies. learnt about the login logout cycle using browser cookies.

---
+++ day-54/server.js
@@ -0,0 +1,88 @@
+// using express
+import express from 'express';
+import path from 'path'
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+
+mongoose.connect("mongodb://localhost:27017", {
+    dbName: 'backend',
+}).then(c => console.log("Connected to DB")).catch(e => console.log(e));
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const app = express();
+
+
+// setting up static folder
+
+// using middleware 
+const staticFile = express.static(path.join(path.resolve(), "public"));
+app.use(staticFile);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+
+// setting up view engine
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+
+    // console.log(req.cookies.token);
+    const { token } = req.cookies;
+    if (token) {
+        res.render("index");
+    } else {
+        res.render("login");
+    }
+
+});
+
+// app.get('/home', (req, res) => {
+//     res.render("index");
+// });
+
+app.post('/login', (req, res) => {
+    res.cookie("token", "1234567890", {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+    res.redirect('/');
+})
+
+app.get('/logout', (req, res) => {
+    res.cookie("token", null, {
+        httpOnly: true,
+        expires: new Date(Date.now()),
+    });
+    res.redirect('/');
+})
+
+
+app.get('/success', (req, res) => {
+    res.render("success");
+});
+
+
+app.post('/contact', async (req, res) => {
+    const { name, email } = req.body;
+    const user = { name, email }
+    await User.create(user);
+    res.redirect('/success');
+})
+
+app.get('/users', (req, res) => {
+    res.json({
+        data,
+    })
+})
+
+app.listen(5500, () => {
+    console.log('Server is listening on port 5500...');
+});
