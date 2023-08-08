b65be2d5cc5699b3f9c54b3a861a783e2f277314
Abhishek <codmonk21@gmail.com>
2023-08-08 17:53:58 +0530

    day-64 commit
    API created for CRUD implementation  of the todo app

---
+++ day-63/controllers/task.js
@@ -0,0 +1,72 @@
+import { Task } from "../models/task.js";
+
+
+export const newTask = async (req, res, next) => {
+
+    const { title, description } = req.body;
+
+    await Task.create({
+        title,
+        description,
+        user: req.user,
+    });
+
+    res.status(201).json({
+        success: true,
+        message: "Task created successfully",
+    });
+
+};
+
+export const getMyTask = async (req, res, next) => {
+
+    const userId = req.user._id;
+    const tasks = await Task.find({ user: userId });
+
+    res.status(200).json({
+        success: true,
+        tasks,
+    });
+
+};
+
+export const updateTask = async (req, res, next) => {
+
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+        return res.status(404).json({
+            success: false,
+            message: "Task not found",
+        });
+    }
+
+    task.isCompleted = !task.isCompleted;
+    await task.save();
+
+    res.status(200).json({
+        success: true,
+        message: "Task updated successfully",
+    });
+
+};
+
+export const deleteTask = async (req, res, next) => {
+
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+        return res.status(404).json({
+            success: false,
+            message: "Task not found",
+        });
+    }
+
+    await task.deleteOne();
+
+    res.status(200).json({
+        success: true,
+        message: "Task Deleted successfully",
+    });
+
+};
