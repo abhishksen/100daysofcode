40abe4a928e9fbbf2024109557b56eb42819f7d5
Abhishek <codmonk21@gmail.com>
2023-08-20 18:47:55 +0530

    day-71 final commit
    home page created

--- project-1/client/src/components/Home.jsx
+++ project-1/client/src/components/Home.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Box, Button, HStack, Heading, Image, Stack, Text, VStack } from '@chakra-ui/react'
+import '../styles/home.css'
+import bg from '../assets/images/bg.png'
+import intro from '../assets/videos/intro.mp4'
+import { FaReact, FaNodeJs, FaFigma } from 'react-icons/fa'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="container">
+        <Stack
+          direction={["column", "row"]}
+          height="100%"
+          justifyContent={["center", "space-between"]}
+          alignItems="center"
+          spacing={["16", "56"]}
+        >
+          <VStack
+            width={"full"}
+            // alignItems={["center", "flex-start"]}
+            alignItems={"flex-start"}
+          >
+            <Heading children="LEARN FROM THE INDUSTRY EXPERTS" size={'2xl'} />
+            <Text children="Find a new way of learning with interactive coursewok." />
+            <Link to="/courses">
+              <Button size={"lg"} colorScheme='purple'>
+                Get Started
+              </Button>
+            </Link>
+          </VStack>
+
+          <Image className='bg_img' boxSize={'md'} src={bg} objectFit="contain" />
+        </Stack>
+      </div>
+      <Box padding={"8"} bg={'blackAlpha.200'}>
+        <Heading textAlign={"center"} fontFamily={"body"} paddingBottom={"4"} children="Popular Courses" />
+        <HStack className='skillBanner' justifyContent={"space-evenly"} marginTop={"7"}>
+          <FaReact />
+          <FaNodeJs />
+          <FaFigma />
+          <FaReact />
+          <FaNodeJs />
+        </HStack>
+      </Box>
+      <div className="vidContainer">
+        <video
+          src={intro}
+          autoPlay={true}
+          controls
+          controlsList='nodownload nofullscreen noremoteplayback'
+          disablePictureInPicture
+          disableRemotePlayback
+        ></video>
+      </div>
+    </section>
   )
 }
 
