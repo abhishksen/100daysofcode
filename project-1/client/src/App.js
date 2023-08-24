8a946a5c00de8beaac8aa21f7ab8ee7008a9c9e2
Abhishek <codmonk21@gmail.com>
2023-08-24 17:49:46 +0530

    day-75 commit
    created subscription page, payment page, success page, page not found page, and the course details page.

--- project-1/client/src/App.js
+++ project-1/client/src/App.js
@@ -11,13 +11,25 @@ import ResetPassword from './components/auth/ResetPassword';
 import Contact from './components/contact/Contact';
 import About from './components/about/About';
 import Request from './components/request/Request';
+import Subscribe from './components/payments/Subscribe';
+import Success from './components/payments/Success';
+import Failed from './components/payments/Failed';
+import NotFound from './components/layout/notFound/NotFound';
+import CoursePage from './components/coursePage/CoursePage';
+
 function App() {
+
+  window.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+  });
+
   return (
     <Router>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/courses' element={<Courses />} />
+        <Route path='/course/:id' element={<CoursePage />} />
         <Route path='/request' element={<Request />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/about' element={<About />} />
@@ -25,6 +37,10 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/forgetpassword' element={<ForgetPassword />} />
         <Route path='/resetpassword/:token' element={<ResetPassword />} />
+        <Route path='/subscribe' element={<Subscribe />} />
+        <Route path='/paymentsuccess' element={<Success />} />
+        <Route path='/paymentfailed' element={<Failed />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
       <Footer />
     </Router>
@@ -32,3 +48,7 @@ function App() {
 }
 
 export default App;
+
+// remaining
+// profile 
+// admin panel
